849da48fe77b8ed09a0c0287621a91830d166b79
truongdai2004 <tmh.dai04@gmail.com>
2026-04-24 03:24:11 +0700

    banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,21 @@ $('.set-bg').each(function () {
     var bg = $(this).data('setbg');
     $(this).css('background-image', 'url(' + bg + ')');
 });
+$(document).ready(function () {
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    var bannerSlider = $(".banner_slider");
+
+    bannerSlider.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1500,
+        dots: true,
+        nav: false,
+    });
+});
